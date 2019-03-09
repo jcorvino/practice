2c4ad44a61fed54d5f6d548fc7055483d7ca10a2
jcorvino <jcorvino@users.noreply.github.com>
2019-03-09 10:50:51 -0500

    Removed numpy import to test pre-commit hook.

--- cracking_the_coding_interview/chapter1/problem3/test_git_hooks.py
+++ cracking_the_coding_interview/chapter1/problem3/test_git_hooks.py
@@ -1,8 +1,4 @@
 """
 Test Git pre-commit hook to auto-update requirements.txt
 """
-import numpy  # import some package to change requirements.txt
 
-print(numpy.pi)
-print(numpy.pi)
-print(numpy.pi)
