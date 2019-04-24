1b89902e334d3ac0164c22af1ea4f840989d49c4
jcorvino <jcorvino@users.noreply.github.com>
2019-04-23 21:58:42 -0400

    Updated problem 2.1 so remove_dups_hashable uses dict as a buffer instead of list. This reduces time complexity from O(N**2) to O(N). (Checking if a key is in a dict takes O(1) time rather than O(N) time for using "in" operator on a list). However, this is only possible if all linked list data is hashable (e.g. no lists allowed).

--- cracking_the_coding_interview/chapter2/problem1/remove_dups.py
+++ cracking_the_coding_interview/chapter2/problem1/remove_dups.py
@@ -1,11 +1,12 @@
 """
 Problem 2.1: Write code to remove duplicates from an unsorted linked list.
+Follow up: What if you are not allowed to use buffers?
 """
 
 
 def remove_dups(linkedlist):
     """
-    Remove duplicates from a linked list.
+    Remove duplicates from a linked list. Linked list data can be any type (accepts non-hashable data)
     Time complexity = O(N**2)
 
     :param linkedlist: A SinglyLinkedList
@@ -15,11 +16,34 @@ def remove_dups(linkedlist):
 
     n = linkedlist.head
     while n is not None:
-        if n.data in unique_data:
+        if n.data not in unique_data:
+            unique_data.append(n.data)
+        else:
             prev.next = n.next
             n = prev  # move n back 1 link so the duplicate is ignored
+        prev = n
+        n = n.next
+
+
+def remove_dups_hashable(linkedlist):
+    """
+    Remove duplicates from a linked list. Requires all linked list data to be hashable (e.g. no lists!)
+    Time complexity = O(N)
+
+    :param linkedlist: A SinglyLinkedList (all data in the linked list must be hashable)
+    :return None: Modifies list in place.
+    """
+    unique_data = {}
+
+    n = linkedlist.head
+    while n is not None:
+        try:
+            unique_data[n.data]
+        except KeyError:
+            unique_data[n.data] = None
         else:
-            unique_data.append(n.data)
+            prev.next = n.next
+            n = prev  # move n back 1 link so the duplicate is ignored
         prev = n
         n = n.next
 
--- cracking_the_coding_interview/chapter2/problem1/test_remove_dups.py
+++ cracking_the_coding_interview/chapter2/problem1/test_remove_dups.py
@@ -5,25 +5,49 @@ import unittest
 import copy
 # from cracking_the_coding_interview.chapter2.linkedlist.node import Node
 from cracking_the_coding_interview.chapter2.linkedlist.linkedlist import SinglyLinkedList, DoublyLinkedList
-from .remove_dups import remove_dups
+from .remove_dups import remove_dups, remove_dups_hashable
 
 
 class TestRemoveDups(unittest.TestCase):
 
     def setUp(self):
+        # setUp for remove_dups:
+        #
         # SinglyLinkedList with duplicates
-        self.sll_with_duplicates = SinglyLinkedList()
-        self.linked_list_data = [1, 1, 2, 3, 2, 3, ['abc'], ['abc']]
-        for data in self.linked_list_data:
-            self.sll_with_duplicates.append(data)
+        self.sll = SinglyLinkedList()
+        linked_list_data = [1, 1, 2, 3, 2, 3, ['abc'], ['abc']]
+        for data in linked_list_data:
+            self.sll.append(data)
 
         # Solution (duplicates removed)
-        self.sll_solution = SinglyLinkedList()
-        self.linked_list_solution_data = [1, 2, 3, ['abc']]
-        for data in self.linked_list_solution_data:
+        self.sll_solution = SinglyLinkedList()  # solution for non-hashable case
+        linked_list_solution_data = [1, 2, 3, ['abc']]
+        for data in linked_list_solution_data:
             self.sll_solution.append(data)
 
+        # setUp for remove_dups_hashable:
+        #
+        # SinglyLinkedList with hashable data
+        self.sll_hashable = SinglyLinkedList()
+        hashable_linked_list_data = [1, 1, 2, 3, 2, 3, ('abc', 'def'), ('abc', 'def')]
+        for data in hashable_linked_list_data:
+            self.sll_hashable.append(data)
+
+        # Hashable solution (duplicates removed)
+        self.sll_hashable_solution = SinglyLinkedList()  # solution for non-hashable case
+        linked_list_solution_data = [1, 2, 3, ('abc', 'def')]
+        for data in linked_list_solution_data:
+            self.sll_hashable_solution.append(data)
+
     def test_remove_dups(self):
-        self.assertNotEqual(self.sll_with_duplicates, self.sll_solution)
-        remove_dups(self.sll_with_duplicates)
-        self.assertEqual(self.sll_with_duplicates, self.sll_solution)
+        # Test remove_dups function
+        self.assertNotEqual(self.sll, self.sll_solution)
+        remove_dups(self.sll)
+        self.assertEqual(self.sll, self.sll_solution)
+
+    def test_remove_dups_hashable(self):
+        # Test remove_dups hashable function
+        # Requires hashable data
+        self.assertNotEqual(self.sll_hashable, self.sll_hashable_solution)
+        remove_dups_hashable(self.sll_hashable)
+        self.assertEqual(self.sll_hashable, self.sll_hashable_solution)
